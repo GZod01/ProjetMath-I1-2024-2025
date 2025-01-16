059ba3596250f6a0160e20c2ddc0292ba1a58a43
GZod01 <gzod01@gzod01.fr>
2025-01-16 09:15:31 +0100

    a

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-header('Content-Type:text/html;charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 mb_internal_encoding("utf-8");
 ?>
 <title>SuperEncrypt</title>
@@ -17,6 +17,169 @@ $char_codes = [];
 foreach ($char_codes_list as $i => $cc) {
     $char_codes[$cc] = $i;
 }
+
+function showMatrix($matrix)
+{
+    echo strMatrix($matrix);
+}
+
+function strMatrix($t)
+{
+    $stroutput = "<table border=1>";
+    foreach ($t as $row) {
+        $stroutput .= "<tr>";
+        if (is_array($row)) {
+            foreach ($row as $cell) {
+                $stroutput .= "<td>$cell</td>";
+            }
+        } else {
+            $stroutput .= "<td>$row</td>";
+        }
+        $stroutput .= "</tr>";
+    }
+    $stroutput .= "</table>";
+    return $stroutput;
+}
+
+function matrixMul(array $mat, array $pair)
+{
+    if (sizeof($pair) != sizeof($mat)) {
+        trigger_error("Pair must have size of Mat", E_USER_ERROR);
+    }
+    $res = array_fill(0, sizeof($pair), 0);
+    for ($i = 0; $i < sizeof($pair); $i++) {
+        for ($j = 0; $j < sizeof($pair); $j++) {
+            $res[$i] += $mat[$i][$j] * $pair[$j];
+        }
+    }
+    return $res;
+}
+
+function showPair($pair)
+{
+    echo strMatrix($pair);
+}
+
+function charCodePair($pair)
+{
+    global $char_codes_list;
+    $npair = [];
+    for ($i = 0; $i < sizeof($pair); $i++) {
+        $index = round(mod27($pair[$i])); // Arrondir l'index
+        $npair[$i] = $char_codes_list[$index];
+    }
+    return $npair;
+}
+
+function rCharCodePair($pair)
+{
+    global $char_codes;
+    $npair = [];
+    for ($i = 0; $i < sizeof($pair); $i++) {
+        $char = $pair[$i];
+        if (isset($char_codes[$char])) {
+            $npair[$i] = round($char_codes[$char]); // Affecter directement la valeur numérique
+        } else {
+            $npair[$i] = 0; // Ou une autre valeur par défaut
+        }
+    }
+    return $npair;
+}
+
+function showMultiply($matrix_a, $pair_b)
+{
+    global $char_codes_list;
+    showMatrix($matrix_a);
+    echo "*";
+    showPair(charCodePair($pair_b));
+    echo "=";
+    showMatrix($matrix_a);
+    echo "*";
+    showPair($pair_b);
+    echo "=";
+    $retPair = matrixMul($matrix_a, $pair_b);
+    showPair($retPair);
+    echo "=";
+    showPair(charCodePair($retPair));
+}
+
+function multiply(array $matrix_a, array $pair_b)
+{
+    showMultiply($matrix_a, $pair_b);
+    return matrixMul($matrix_a, $pair_b);
+}
+
+function mod27($a)
+{
+    global $charlist;
+    $mod_val = mb_strlen($charlist);
+    return fmod(($mod_val + fmod($a, $mod_val)), $mod_val); // Utiliser fmod() pour les nombres à virgule flottante
+}
+function mod27Mat($pair)
+{
+    $npair = [];
+    for ($i = 0; $i < sizeof($pair); $i++) {
+        $npair[$i] = mod27($pair[$i]);
+    }
+    return $npair;
+}
+
+function encrypt($message, $base, $rows_amount)
+{
+    global $char_codes, $char_codes_list;
+    $message_encrypted = "";
+    $ccpairslist = [];
+    echo "<div class=messagepairs>";
+    echo "message: $message ";
+    echo "in pairs = ";
+    foreach (mb_str_split($message, $rows_amount) as $char) {
+        echo "<div class=equationstart>";
+        $chars = mb_str_split($char);
+        $char_codes_pair = rCharCodePair($chars);
+        $ccpairslist[] = $char_codes_pair;
+        showPair($chars);
+        echo "=>";
+        showPair($char_codes_pair);
+        echo "</div>";
+    }
+    echo "</div>";
+    foreach ($ccpairslist as $char_codes_pair) {
+        echo "<div class=equationstart>";
+        $char_codes_encrypted = multiply($base, $char_codes_pair);
+        $char_codes_encrypted = mod27Mat($char_codes_encrypted);
+        $message_encrypted .= implode("", charCodePair($char_codes_encrypted));
+        echo "</div>";
+    }
+    return $message_encrypted;
+}
+
+function decrypt($message, $decrypt_base, $rows_amount)
+{
+    return encrypt($message, $decrypt_base, $rows_amount);
+}
+
+// Fonction pour calculer le PGCD (Plus Grand Commun Diviseur)
+function pgcd($a, $b)
+{
+    while ($b != 0) {
+        $temp = $a % $b;
+        $a = $b;
+        $b = $temp;
+    }
+    return $a;
+}
+
+// Fonction pour calculer l'inverse modulaire
+function inverseModulaire($a, $m)
+{
+    for ($x = 1; $x < $m; $x++) {
+        if (($a * $x) % $m == 1) {
+            return $x;
+        }
+    }
+    return null;
+}
+
 function modifierMatriceAvecPremiers($matrice)
 {
     global $charlist;
@@ -50,134 +213,6 @@ function modifierMatriceAvecPremiers($matrice)
     return $matrice;
 }
 
-
-function showMatrix($matrix)
-{
-    echo strMatrix($matrix);
-}
-function strMatrix($t)
-{
-    $stroutput = "<table border=1>";
-    foreach ($t as $row) {
-        $stroutput .= "<tr>";
-        if (is_array($row)) {
-            foreach ($row as $cell) {
-                $stroutput .= "<td>$cell</td>";
-            }
-        } else {
-            $stroutput .= "<td>$row</td>";
-        }
-        $stroutput .= "</tr>";
-    }
-    $stroutput .= "</table>";
-    return $stroutput;
-}
-function matrixMul(array $mat, array $pair)
-{
-    if (sizeof($pair) != sizeof($mat)) {
-        trigger_error("Pair must have size of Mat", E_USER_ERROR);
-    }
-    $res = array_fill(0, sizeof($pair), 0);
-    for ($i = 0; $i < sizeof($pair); $i++) {
-        for ($j = 0; $j < sizeof($pair); $j++) {
-            $res[$i] += $mat[$i][$j] * $pair[$j]; // Correction : += au lieu de =
-        }
-    }
-    return $res;
-}
-function showPair($pair)
-{
-    echo strMatrix($pair);
-}
-function charCodePair($pair)
-{
-    global $char_codes_list;
-    $npair = [];
-    for ($i = 0; $i < sizeof($pair); $i++) {
-        $npair[$i] = $char_codes_list[mod27($pair[$i])];
-    }
-    return $npair;
-}
-function rCharCodePair($pair)
-{
-    global $char_codes;
-    $npair = [];
-    for ($i = 0; $i < sizeof($pair); $i++) {
-        $npair[$i] = $char_codes[$pair[$i]];
-    }
-    return $npair;
-}
-function showMultiply($matrix_a, $pair_b)
-{
-    global $char_codes_list;
-    showMatrix($matrix_a);
-    echo "*";
-    showPair(charCodePair($pair_b));
-    echo "=";
-    showMatrix($matrix_a);
-    echo "*";
-    showPair($pair_b);
-    echo "=";
-    $retPair = matrixMul($matrix_a, $pair_b);
-    showPair($retPair);
-    echo "=";
-    showPair(charCodePair($retPair));
-}
-
-function multiply(array $matrix_a, array $pair_b)
-{
-    showMultiply($matrix_a, $pair_b);
-    return matrixMul($matrix_a, $pair_b);
-}
-function mod27($a)
-{
-    global $charlist;
-    $mod_val = mb_strlen($charlist) - 1;
-    return ($mod_val + ($a % $mod_val)) % $mod_val;
-}
-function mod27Mat($pair)
-{
-    $npair = [];
-    for ($i = 0; $i < sizeof($pair); $i++) {
-        $npair[$i] = mod27($pair[$i]);
-    }
-    return $npair;
-}
-function encrypt($message, $base, $rows_amount)
-{
-    global $char_codes, $char_codes_list;
-    $message_encrypted = "";
-    $ccpairslist = [];
-    echo "<div class=messagepairs>";
-    echo "message: $message ";
-    echo "in pairs = ";
-    // echo "<wbr/>";
-    foreach (mb_str_split($message, $rows_amount) as $char) {
-        echo "<div class=equationstart>";
-        $chars = mb_str_split($char);
-        $char_codes_pair = rCharCodePair($chars);
-        $ccpairslist[] = $char_codes_pair;
-        showPair($chars);
-        echo "=>";
-        showPair($char_codes_pair);
-        // echo "<wbr/>";
-        echo "</div>";
-    }
-    echo "</div>";
-    foreach ($ccpairslist as $char_codes_pair) {
-        echo "<div class=equationstart>";
-        $char_codes_encrypted = multiply($base, $char_codes_pair);
-        $char_codes_encrypted = mod27Mat($char_codes_encrypted);
-        $message_encrypted .= implode("", charCodePair($char_codes_encrypted));
-        echo "</div>";
-    }
-    return $message_encrypted;
-}
-
-function decrypt($message, $decrypt_base, $rows_amount)
-{
-    return encrypt($message, $decrypt_base, $rows_amount);
-}
 $base = [[1, 2], [3, 7]];
 $rows_amount = 2;
 $message;
@@ -186,9 +221,11 @@ $echostr = "";
 $preoutput = "";
 ob_start();
 $baseArr = [1, 2, 3, 7];
+
 if (isset($_REQUEST["rows_amount"])) {
     $rows_amount = $_REQUEST["rows_amount"];
 }
+
 if (isset($_REQUEST["base"])) {
     if (is_array($_REQUEST["base"])) {
         $baseArr = castIntArray($_REQUEST["base"]);
@@ -197,6 +234,7 @@ if (isset($_REQUEST["base"])) {
         $baseArr = castIntArray(str_contains(",", $_REQUEST["base"]) ? explode(",", $_REQUEST["base"]) : str_split($_REQUEST["base"]));
     }
 }
+
 function castIntArray($arr)
 {
     $narr = [];
@@ -232,13 +270,13 @@ function calculDet($mat)
 
     return $det;
 }
+
 function arrToMatrix($baseArr, $n)
 {
     $base = [];
     $k = 0;
     for ($i = 0; $i < $n; $i++) {
         for ($j = 0; $j < $n; $j++) {
-            // echo $k;
             $base[$i][$j] = $baseArr[$k];
             $k++;
         }
@@ -268,8 +306,7 @@ function matInv($base, $n)
 
         // Vérifier si l'inverse modulaire existe
         if ($invDet === null) {
-            trigger_error("Erreur : La matrice n'est pas inversible.", E_USER_ERROR);
-            return;
+            return null;
         }
 
         // Multiplier chaque élément de la matrice inverse par l'inverse modulaire
@@ -309,14 +346,14 @@ function matInv($base, $n)
                     }
                 }
                 if ($pivot == 0) {
-                    trigger_error("Erreur : La matrice n'est pas inversible.", E_USER_ERROR);
-                    return;
+                    return null;
                 }
             }
 
             // Normaliser la ligne du pivot
+            $inversePivot = inverseModulaire($pivot, mb_strlen($charlist)); // Calculer l'inverse du pivot
             for ($j = 0; $j < 2 * $n; $j++) {
-                $augmentee[$i][$j] /= $pivot;
+                $augmentee[$i][$j] = mod27($augmentee[$i][$j] * $inversePivot); // Multiplier par l'inverse du pivot
             }
 
             // Éliminer les autres éléments de la colonne du pivot
@@ -324,7 +361,7 @@ function matInv($base, $n)
                 if ($k != $i) {
                     $facteur = $augmentee[$k][$i];
                     for ($j = 0; $j < 2 * $n; $j++) {
-                        $augmentee[$k][$j] -= $facteur * $augmentee[$i][$j];
+                        $augmentee[$k][$j] = mod27($augmentee[$k][$j] - $facteur * $augmentee[$i][$j]);
                     }
                 }
             }
@@ -344,8 +381,7 @@ function matInv($base, $n)
 
         // Vérifier si l'inverse modulaire existe
         if ($invDet === null) {
-            trigger_error("Erreur : La matrice n'est pas inversible.", E_USER_ERROR);
-            return;
+            return null;
         }
 
         // Multiplier chaque élément de la matrice inverse par l'inverse modulaire
@@ -357,15 +393,6 @@ function matInv($base, $n)
 
         return $inverse;
     }
-}
-function inverseModulaire($a, $m)
-{
-    for ($x = 1; $x < $m; $x++) {
-        if (($a * $x) % $m == 1) {
-            return $x;
-        }
-    }
-    return null;
 }
 function minor($base, $i, $j)
 {
@@ -380,35 +407,33 @@ function minor($base, $i, $j)
     }
     return $minor;
 }
+
 if (isset($_REQUEST["message"]) and $_REQUEST["message"] != "") {
     if (sizeof($baseArr) < $rows_amount) {
         echo "bad base, must have rows_amount rows<br>\n";
         $baseArr = [1, 2, 3, 7];
         $rows_amount = 2;
     }
-    // if (inverseModulaire(calculDet($base, $rows_amount), mb_strlen($charlist)) === null) {
-    //     echo "mat not inversible";
-    //     $baseArr = [1, 2, 3, 7];
-    //     $rows_amount = 2;
-    // }
-    $baseStr = implode(",", $baseArr);
 
+    $baseStr = implode(",", $baseArr);
     $base = arrToMatrix($baseArr, $rows_amount);
-    $baseInv = matInv($base, $rows_amount);
-    if($baseInv === null){
-        echo "mat not inversible";
-        $baseArr = [1, 2, 3, 7];
-        $rows_amount = 2;
-        $base = arrToMatrix($baseArr, $rows_amount);
-        $baseInv = matInv($base, $rows_amount);
+
+    // Modification de la matrice avec des nombres premiers si elle n'est pas inversible
+    if (calculDet($base, $rows_amount) == 0) {
+        echo "La matrice n'est pas inversible. Modification avec des nombres premiers...<br>";
+        $base = modifierMatriceAvecPremiers($base);
     }
+
+    $baseInv = matInv($base, $rows_amount);
     $message = $_REQUEST["message"];
     $n_message = "";
+
     foreach (mb_str_split($message) as $char) {
         if (!isset($char_codes[$char])) $char = iconv('UTF-8', 'ASCII//TRANSLIT', $char);
         if (!isset($char_codes[$char])) $char = " ";
         $n_message .= $char;
     }
+
     while (mb_strlen($n_message) % $rows_amount != 0) $n_message .= " ";
     $message = $n_message;
     $action = $_REQUEST["action"] ?? "encrypt";
@@ -426,6 +451,7 @@ if (isset($_REQUEST["message"]) and $_REQUEST["message"] != "") {
     $echostr .= "<fieldset class=base><legend>Reverse Base:</legend><div class=equationstart>" . strMatrix($baseInv) . "</div></fieldset></div>";
     $echostr .= "<fieldset class=encrypted>";
     $echostr .= "<legend>Result message</legend>";
+
     if ($action == "decrypt") {
         $echostr .= decrypt($message, $baseInv, $rows_amount);
     } else {
@@ -435,6 +461,7 @@ if (isset($_REQUEST["message"]) and $_REQUEST["message"] != "") {
     $echostr .= "</div>";
     echo "</div>";
 }
+
 $preoutput = ob_get_clean();
 ob_end_clean();
 ?>
@@ -446,7 +473,6 @@ ob_end_clean();
             <input type=text name="message" value="<?= htmlspecialchars($message ?? "") ?>">
             <label for=rows_amount>Nombre de colonnes (appuyer sur encrypter pour actualiser la vue d'entrée de la matrice ci-dessous):
                 <input type=number name="rows_amount" value="<?= htmlspecialchars($rows_amount) ?>" required=true></label>
-            <!-- <input type=text name="base" value="<?= htmlspecialchars($baseStr ?? "1,2,3,7") ?>" required=false optional> -->
             <div class=equationstart>
                 <table>
                     <?php for ($i = 0; $i < $rows_amount; $i++) { ?>
@@ -465,93 +491,92 @@ ob_end_clean();
     <details class=preoutput>
         <summary>show debug</summary>
         <?= $preoutput ?>
-        </div>
+    </details>
 
-        <style>
-            body {
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            width: 100%;
+            margin: 0px;
+            padding: 0px;
+            background-color: #f0f0f0;
+        }
+
+        .preoutput {
+            max-width: 80%;
+            width: 80%;
+            border: 2px solid black;
+            border-radius: 5px;
+            padding: 10px;
+            margin: 10px;
+
+            .messagepairs {
                 display: flex;
-                flex-direction: column;
-                justify-content: center;
+                flex-direction: row;
+                flex-wrap: wrap;
                 align-items: center;
-                min-height: 100vh;
-                width: 100%;
-                margin: 0px;
-                padding: 0px;
-                background-color: #f0f0f0;
+                border: 1px solid black;
             }
+        }
 
-            .preoutput {
+        .superLine {
+            display: flex;
+            flex-direction: row;
+        }
 
-                max-width: 80%;
-                width: 80%;
+        form {
+            display: flex;
+            justify-content: center;
+            margin: 10px;
+            flex-direction: column;
+
+            input {
+                margin: 5px;
+                padding: 10px;
+                border-radius: 5px;
+                border: 1px solid black;
+            }
+        }
+
+        .equationstart {
+            display: flex;
+            /* justify-content: center; */
+            align-items: center;
+            margin: 10px;
+
+            table {
+                display: block;
+                margin: 0 10px;
+                border-collapse: collapse;
                 border: 2px solid black;
                 border-radius: 5px;
-                padding: 10px;
-                margin: 10px;
+                border-top: 0px solid transparent;
+                border-bottom: 0px solid transparent;
 
-                .messagepairs {
-                    display: flex;
-                    flex-direction: row;
-                    flex-wrap: wrap;
-                    align-items: center;
-                    border: 1px solid black;
+                td {
+                    padding: 0 5px;
+                    border: 0px solid transparent;
+                    text-align: center;
                 }
             }
+        }
 
-            .superLine {
+        .result {
+            display: flex;
+            border: 2px solid black;
+            border-radius: 5px;
+            flex-direction: column;
+            padding: 10px;
+            margin: 10px;
+
+            .lineBases {
                 display: flex;
                 flex-direction: row;
             }
-
-            form {
-                display: flex;
-                justify-content: center;
-                margin: 10px;
-                flex-direction: column;
-
-                input {
-                    margin: 5px;
-                    padding: 10px;
-                    border-radius: 5px;
-                    border: 1px solid black;
-                }
-            }
-
-            .equationstart {
-                display: flex;
-                /* justify-content: center; */
-                align-items: center;
-                margin: 10px;
-
-                table {
-                    display: block;
-                    margin: 0 10px;
-                    border-collapse: collapse;
-                    border: 2px solid black;
-                    border-radius: 5px;
-                    border-top: 0px solid transparent;
-                    border-bottom: 0px solid transparent;
-
-                    td {
-                        padding: 0 5px;
-                        border: 0px solid transparent;
-                        text-align: center;
-                    }
-                }
-            }
-
-            .result {
-                display: flex;
-                border: 2px solid black;
-                border-radius: 5px;
-                flex-direction: column;
-                padding: 10px;
-                margin: 10px;
-
-                .lineBases {
-                    display: flex;
-                    flex-direction: row;
-                }
-            }
-        </style>
+        }
+    </style>
 </body>
